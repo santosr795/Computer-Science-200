b679fa4742f8dc2497223db1c093bb37f44ddbc7
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2018-09-30 23:53:45 -0400

    Create FindTheRoot.java

---
+++ FindTheRoot.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class FindTheRoot {
+private String coefficient5; 
+private String coefficient4;
+private String coefficient3; 
+private String coefficient2; 
+private String coefficient1; 
+private double constant; 
+
+FindTheRoot(){
+	this.coefficient5 = coefficient5; 
+	this.coefficient4 = coefficient4; 
+	this.coefficient3 = coefficient3; 
+	this.coefficient2 = coefficient2; 
+	this.coefficient1 = coefficient1; 
+	this.constant = constant; 
+	
+}
+
+}//End class of FindTheRoot. 
